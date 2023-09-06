f915dc69628f1d23051aea56d677f8645d170a2d
Alex Maina <lexizgichbuoy@gmail.com>
2023-09-06 03:58:42 +0300

    feat: added_favicon

--- assets/js/main.js
+++ assets/js/main.js
@@ -191,3 +191,19 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+// FORM SUBMIT
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
